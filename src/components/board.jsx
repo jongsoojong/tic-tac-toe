60ab6ab33db83b7f777ab0f6e1f4d7ea0de682d6
Jongsoo <jongsoojong@gmail.com>
2021-12-18 16:41:55 -0800

    Basic Functionality

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -4,30 +4,94 @@ import { Row } from './row';
 export const Board = () => {
 
     const initialState = [
-        [1,2,3],
-        [4,5,6],
-        [7,8,9]
+        [null,null,null],
+        [null,null,null],
+        [null,null,null]
     ];
+    const initialTurn = 1;
+    const initialPlayer = 'X'
+    const initialWin = null;
+
+    const[winState, setWinState] = useState(null);
+
+    const[turnState, setTurnState] = useState(1);
 
     const [gameBoard, setGameBoard] = useState( initialState )
+
+    const [playerState, setPlayerState] = useState('X');
 
     const handleClick = (rowId, cellId) => {
         // DEEP COPY REQUIRED DUE TO MATRIX
         const updatedGameboard = JSON.parse(JSON.stringify(gameBoard));
-;
+        let updatedTurnState = turnState;
 
-        updatedGameboard[rowId][cellId] = (gameBoard[rowId][cellId] * 2);
+        //Set the player State
+        if(updatedGameboard[rowId][cellId] === 'X' || updatedGameboard[rowId][cellId] === 'O' || winState) {
+            //do nothing
+        } else {
+            updatedGameboard[rowId][cellId] = playerState
+            setGameBoard(updatedGameboard);
+            setPlayerState(playerState === 'X' ? 'O' : 'X');
+            updatedTurnState++
+            setTurnState(updatedTurnState);
+        }
 
-        setGameBoard(updatedGameboard);
+    }
+
+    const handleReset = () => {
+        setGameBoard(initialState);
+        setTurnState(initialTurn);
+        setPlayerState(initialPlayer);
+        setWinState(initialWin);
     }
 
     useEffect(() => {
-        console.log('board changed');
+        //Horizontal
+        if(gameBoard[0][0] === gameBoard[0][1] && gameBoard[0][1] === gameBoard[0][2] && gameBoard[0][1])  {
+            setWinState(gameBoard[0][0])
+        }
+        if(gameBoard[1][0] === gameBoard[1][1] && gameBoard[1][1] === gameBoard[1][2] && gameBoard[1][1])  {
+            setWinState(gameBoard[1][0])
+        }
+        if(gameBoard[2][0] === gameBoard[2][1] && gameBoard[2][1] === gameBoard[2][2]  && gameBoard[2][1])  {
+            setWinState(gameBoard[2][0])
+        }
+        //Vertical
+        if(gameBoard[0][0] === gameBoard[1][0] && gameBoard[1][0] === gameBoard[2][0] && gameBoard[1][0])  {
+            setWinState(gameBoard[0][0])
+        }
+        if(gameBoard[0][1] === gameBoard[1][1] && gameBoard[1][1] === gameBoard[2][1] && gameBoard[1][1])  {
+            setWinState(gameBoard[0][1])
+        }
+        if(gameBoard[0][2] === gameBoard[1][2] && gameBoard[1][2] === gameBoard[2][2] && gameBoard[1][2])  {
+            setWinState(gameBoard[0][2])
+        }
+
+        //Diagonal
+        if(gameBoard[0][0] === gameBoard[1][1] && gameBoard[1][1] === gameBoard[2][2] && gameBoard[1][1])  {
+            setWinState(gameBoard[1][1])
+
+        }
+        if(gameBoard[0][2] === gameBoard[1][1] && gameBoard[1][1] === gameBoard[2][0] && gameBoard[1][1])  {
+            setWinState(gameBoard[1][1])
+        }
+
+
     }, [gameBoard]);
 
     return (
         
         <div className="board">
+            <div className="player">
+                {
+                    winState && `${winState} Wins!` 
+                }
+            </div>
+            <div className="turnCount">
+                {
+                    (turnState === 10 && !winState) ? 'TIE' : `Turn: ${turnState}`
+                }
+            </div>
             {
                 gameBoard.map((row, rowIndex) => {
                     return (
@@ -40,6 +104,10 @@ export const Board = () => {
                     );
                 })
             }
+
+            <div>
+                <button className="resetButton" onClick={ handleReset}>Reset</button>
+            </div>
            
         </div>
     )
--- src/components/cell.jsx
+++ src/components/cell.jsx
@@ -2,14 +2,6 @@ import React from 'react';
 
 
 export const Cell = ({ cellValue, rowId, cellIndex, handleClick }) => {
-    const clickFunction = () => {
-        console.log('THIS IS MY NUMBER', cellValue );
-        console.log('THIS IS MY ROW', rowId );
-        console.log('THIS IS MY CELL INDEX', cellIndex)
-
-
-    }
-
 
     return (
         <div className="cell" 
